c9fce9a2197e80adc06dd3b59ea5618531b3f4ee
yana <begemotvot2@gmail.com>
2022-10-24 16:42:30 +0300

    Created a "hello world" API view

--- app/main.py
+++ app/main.py
@@ -5,5 +5,10 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8000, host="0.0.0.0", reload=True)
