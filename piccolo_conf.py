a3e81f152cf04a6bb78d13c70a08a715ea90a09c
Romanskyy Andrey <andrey.romanskyy@icloud.com>
2022-10-26 00:00:57 +0300

    {#18} Create piccolo app
    Created new piccolo app named bot_app with base
    files' structure inside.
    
    Signed-off-by: Romanskyy Andrey <andrey.romanskyy@icloud.com>

--- piccolo_conf.py
+++ piccolo_conf.py
@@ -1,8 +1,20 @@
 from piccolo.conf.apps import AppRegistry
 from piccolo.engine.postgres import PostgresEngine
 
+from dotenv import load_dotenv
 
-DB = PostgresEngine(config={'database': 'flash-cards-chatbot'})
+import os
+
+load_dotenv()
+
+db_pass = os.getenv('POSTGRES_PASSWORD')
+
+DB = PostgresEngine(config={
+    'host': 'localhost',
+    'database': 'flash-cards-chatbot',
+    'user': 'postgres',
+    'password': db_pass,
+})
 
 
 # A list of paths to piccolo apps
