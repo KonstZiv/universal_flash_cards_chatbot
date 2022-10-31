5b98dfc60369e3556ac858719e17f3c2682977f2
UlkaBo <ul.bozhko@gmail.com>
2022-10-31 14:32:10 +0200

    add aiogram and dotenv to .toml , add different keyboard to project

--- app/keyboards.py
+++ app/keyboards.py
@@ -1,9 +1,28 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+languages = ["Albanian", "Belarusian", "Chinese",
+             "Croatian", "English", "Estonian",
+             "French", "German", "Russian",
+             "Ukrainian", ]
 
-markup_requests = ReplyKeyboardMarkup(resize_keyboard=True)\
-    .add(KeyboardButton('Відіслати власну геолокацію'))\
-    .add(KeyboardButton('Поділитись власним контактом'))
+from aiogram.types import ReplyKeyboardRemove,\
+    ReplyKeyboardMarkup, KeyboardButton, \
+    InlineKeyboardMarkup, InlineKeyboardButton, \
+    KeyboardButtonPollType, PollType
 
-button_parent = KeyboardButton("for my children")
-button_me = KeyboardButton("for me")
-general_kb = ReplyKeyboardMarkup(resize_keyboard=True).add(button_me).add(button_parent)
+markup_down = ReplyKeyboardMarkup(resize_keyboard=True)\
+    .add(KeyboardButton('Add new word'))\
+    .add(KeyboardButton('Train'))
+
+select_language_keyboard = InlineKeyboardMarkup(resize_keyboard=True)
+
+for i in range(0,len(languages)-1,3):
+    select_language_keyboard.add(InlineKeyboardButton(languages[i], callback_data=languages[i]),
+                                 InlineKeyboardButton(languages[i+1], callback_data=languages[i+1]),
+                                 InlineKeyboardButton(languages[i+2], callback_data=languages[i+2]),)
+select_language_keyboard.add(InlineKeyboardButton(languages[-1], callback_data=languages[-1]))
+
+
+button_parent = InlineKeyboardButton("for my children")
+button_me = InlineKeyboardButton("for me")
+
+
+general_kb = InlineKeyboardMarkup(resize_keyboard=True).add(button_me).add(button_parent)
--- app/main.py
+++ app/main.py
@@ -1,9 +1,61 @@
-import uvicorn
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
 
-from fastapi import FastAPI
 
-app = FastAPI()
+from dotenv import load_dotenv
+import os
 
+import keyboards as kb
+
+load_dotenv()
+TOKEN = os.getenv('BOT_TOKEN')
+ADMIN = 887549275
+
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+native_language = target_language = ''
+
+async def set_default_commands(dp):
+    print('bot command')
+    await dp.bot.set_my_commands(
+        [
+            types.BotCommand('start', 'start'),
+            types.BotCommand('train', 'Запустить бота'),
+        ]
+    )
+    #x = await bot.get_my_commands()
+    #print('get_my_commands', x)
+
+@dp.message_handler(commands = ['start', 'початок',])
+async def start_message(msg: types.Message):
+    await set_default_commands(dp)
+    x = await bot.get_my_commands()
+    print('get_my_commands', x)
+    print(msg)
+    if msg.from_user.id == ADMIN:
+        print('hi, admin')
+    await bot.send_message(msg.from_user.id, text=f"Hello, {msg.from_user.full_name}",  reply_markup=kb.markup_down)
+    await msg.answer(text="what is your native language?", reply_markup=kb.select_language_keyboard)
+
+
+@dp.callback_query_handler(text = kb.languages)
+async def select_target_language(callback_query: types.CallbackQuery):
+    global native_language, target_language
+    print(callback_query)
+    if not native_language:
+        native_language = callback_query.data
+        await bot.send_message(callback_query.from_user.id,
+                                    text="what is your target language?",
+                                    reply_markup=kb.select_language_keyboard)
+
+    else:
+        target_language = callback_query.data
+        await bot.send_message(callback_query.from_user.id,
+                               text=f"{callback_query.from_user.first_name}, your native language is {native_language}, your target - {target_language}")
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=8000, host="0.0.0.0", reload=True)
+
+    executor.start_polling(dp)
